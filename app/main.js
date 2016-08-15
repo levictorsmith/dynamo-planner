9b98ac46a8943c0d25710d1759754c6ba0603c0b
LeVan Smith <levictorsmith@gmail.com>
2016-08-15 09:46:36 -0600

    Added the rest of the html framework. Added the actionbar functions and the modals associated with it.

--- app/main.js
+++ app/main.js
@@ -1,1 +1,20 @@
-var app = angular.module('dynamo-planner', []);
+(function (){
+  var app = angular.module('dynamo-planner', []);
+
+})();
+function newEvent() {
+  $('eventModal').show();
+  console.log("Added Event");
+}
+function newReminder() {
+  $('reminderModal').show();
+  console.log("Added Reminder");
+}
+function newGoal() {
+  $('goalModal').show();
+  console.log("Added Goal");
+}
+function newNote() {
+  $('noteModal').show();
+  console.log("Added Note");
+}
