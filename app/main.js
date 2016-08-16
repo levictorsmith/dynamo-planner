442be1a0642eeb1cb36421ac6737845078888779
LeVan Smith <levictorsmith@gmail.com>
2016-08-15 19:47:31 -0600

    Lots of stuff. Mostly just CSS tweaking and adding the DatePicker.

--- app/main.js
+++ app/main.js
@@ -1,42 +1,76 @@
 (function (){
   var app = angular.module('dynamo-planner', ['ui.bootstrap']);
-  angular.module('dynamo-planner').controller('TimepickerDemoCtrl', function ($scope, $log) {
+  // TIMEPICKER CONTROLLER
+  app.controller('TimepickerDemoCtrl', function ($scope, $log) {
   $scope.mytime = new Date();
-
+  if ($scope.mytime.getMinutes() >= 30 ) {
+      $scope.mytime.setMinutes(30);
+  } else {
+    $scope.mytime.setMinutes( 0 );
+  }
   $scope.hstep = 1;
   $scope.mstep = 15;
-
-  $scope.options = {
-    hstep: [1, 2, 3],
-    mstep: [1, 5, 10, 15, 25, 30]
-  };
-
   $scope.ismeridian = true;
-  $scope.toggleMode = function() {
-    $scope.ismeridian = ! $scope.ismeridian;
-  };
 
-  $scope.update = function() {
-    var d = new Date();
-    d.setHours( 14 );
-    d.setMinutes( 0 );
-    $scope.mytime = d;
-  };
+  $scope.setTime = function() {
 
-  $scope.changed = function () {
-    $log.log('Time changed to: ' + $scope.mytime);
   };
 
   $scope.clear = function() {
     $scope.mytime = null;
   };
 });
+// DATEPICKER CONTROLLER
+app.controller('DatepickerPopupDemoCtrl', function ($scope) {
+  $scope.today = function() {
+    $scope.dt = new Date();
+  };
+  $scope.today();
+  $scope.format = 'shortDate';
 
+  $scope.inlineOptions = {
+    customClass: getDayClass,
+    minDate: new Date(),
+    showWeeks: true
+  };
+
+  $scope.dateOptions = {
+    dateDisabled: disabled,
+    formatYear: 'yy',
+    maxDate: new Date(2030, 5, 22),
+    minDate: new Date(),
+    startingDay: 1
+  };
+
+  function disabled(data) { return false; }
+
+  $scope.open1 = function() { $scope.popup1.opened = true; };
+  $scope.open2 = function() { $scope.popup2.opened = true; };
+
+  $scope.popup1 = { opened: false };
+  $scope.popup2 = { opened: false };
+
+  function getDayClass(data) {
+    var date = data.date,
+      mode = data.mode;
+    if (mode === 'day') {
+      var dayToCheck = new Date(date).setHours(0,0,0,0);
+
+      for (var i = 0; i < $scope.events.length; i++) {
+        var currentDay = new Date($scope.events[i].date).setHours(0,0,0,0);
+
+        if (dayToCheck === currentDay) {
+          return $scope.events[i].status;
+        }
+      }
+    }
+    return '';
+  }
+});
 })();
 
 function newEvent() {
   $('#eventModal').modal('show');
-  console.log("Added Event");
 }
 
 function newReminder() {
@@ -52,4 +86,9 @@ function newGoal() {
 function newNote() {
   $('#noteModal').modal('show');
   console.log("Added Note");
+}
+function addEvent() {
+  //Add badge with number and with type color
+  
+  console.log("Added Event");
 }
